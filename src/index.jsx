d7e1b13d28badea9dcbd9f2fd7b1b678f93d407d
cmf2glass <cmf2glass@gmail.com>
2026-08-26 18:17:20 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './App.css';  // ← 이 라인 추가!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
